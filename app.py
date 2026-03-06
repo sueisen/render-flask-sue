c68b624be0b5273dfe3d47df92a0c4ee0a777193
Sueisen Barraza <117995378+sueisen@users.noreply.github.com>
2026-03-06 09:17:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API funcionando en Render"
+    return "API funcionando en Render para probar el despliegue continuo"
 
 if __name__ == "__main__":
     app.run()
